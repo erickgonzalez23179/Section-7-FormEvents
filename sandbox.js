5467b4fe9024294dffe33e7e866ab81e12946e0e
Erick Gonzalez <23179@columbushs.com>
2021-01-20 12:41:36 -0500

    finished

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,17 @@
 const form = document.querySelector(`.signup-form`);
+const feedback = document.querySelector(`.feedback`)
 // const username = document.querySelector(`#username`);
 
 form.addEventListener(`submit`, e => {
   e.preventDefault();
-  // console.log(username.value)
-  console.log(form.username.value);
+  
+  const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+  if(usernamePattern.test(username)){
+    feedback.textContent = `That username is valid`
+  } else {
+    feedback.textContent = `username must contain letters only and be between 6 and 12 characters`
+  }
 
 })
-
-// testing RegEx
-const username = `12shaunp`
-const pattern = /^[a-z]{6,}$/
-
-// let result = pattern.test(username);
-
-// if(result){
-//   console.log(`regex test passed ;)`);  
-// } else {
-//   console.log(`regex test failed :(`);
-// }
-
-let result = username.search(pattern)
-console.log(result)
