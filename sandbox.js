533cdc1f743216512954722c9e954fd54ea15636
Erick Gonzalez <23179@columbushs.com>
2021-01-20 12:31:41 -0500

    finished

--- sandbox.js
+++ sandbox.js
@@ -7,3 +7,18 @@ form.addEventListener(`submit`, e => {
   console.log(form.username.value);
 
 })
+
+// testing RegEx
+const username = `12shaunp`
+const pattern = /^[a-z]{6,}$/
+
+// let result = pattern.test(username);
+
+// if(result){
+//   console.log(`regex test passed ;)`);  
+// } else {
+//   console.log(`regex test failed :(`);
+// }
+
+let result = username.search(pattern)
+console.log(result)
